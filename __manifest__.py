156304ff1c3cc05488611395008e7fff8f2c597c
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-08-29 22:59:46 +0700

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,39 +1,19 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Library Management",
-
     "summary": "Manage library catalog and book lending.",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
     "author": "Daniel Reis",
-
     "license": "AGPL-3",
-
-    "website":  "https://github.com/PacktPublishing"
-                "/Odoo-15-Development-Essentials",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    "category": "Services/Library",
+    "website": "https://github.com/PacktPublishing"
+               "/Odoo-15-Development-Essentials",
     "version": "15.0.1.0.0",
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-
+    "category": "Services/Library",
+    "depends": ["base"],
+    "data": [
+        "security/library_security.xml",
+        "security/ir.model.access.csv",
+        "views/book_view.xml",
+        "views/library_menu.xml",
+        "views/book_list_template.xml",
+        ],
     "application": True,
 }
