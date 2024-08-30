7c912c21795c610988345c1e4701037cf9a845bf
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-08-30 16:35:07 +0700

    feat - creating a form view

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     "data": [
         "security/library_security.xml",
         "security/ir.model.access.csv",
-        # "views/book_view.xml",
+        "views/book_view.xml",
         "views/library_menu.xml",
         # "views/book_list_template.xml",
         ],
