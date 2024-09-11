7771109fa2404fd3d2738d6e624dfd48bc6c7605
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-09-11 11:03:59 +0700

    feat - embedding models using delegation inheritance

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -6,6 +6,9 @@
   "depends": ["library_app"],
   "application": False,
   "data": [
+    "security/ir.model.access.csv",
     "views/book_view.xml",
+    "views/member_view.xml",
+    "views/library_menu.xml"
   ]
 }
